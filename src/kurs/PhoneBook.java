d507d980506fe79d9dc7889d94ff86a8959f1a1c
YuriDyachenko <yuri.dyachenko@outlook.com>
2021-02-19 01:19:11 +0300

    проверка на уникальность пары при добавлении изменена

--- src/kurs/PhoneBook.java
+++ src/kurs/PhoneBook.java
@@ -8,11 +8,11 @@ public class PhoneBook {
     private final List phones = new ArrayList();
 
     public void add(String name, String phone) {
-        int found = names.indexOf(name);
-        if (found == -1 || !phone.equals(phones.get(found))) {
-            names.add(name);
-            phones.add(phone);
+        for (int i = 0; i < names.size(); i++) {
+            if (name.equals(names.get(i)) && phone.equals(phones.get(i))) return;
         }
+        names.add(name);
+        phones.add(phone);
     }
 
     public List get(String name) {
